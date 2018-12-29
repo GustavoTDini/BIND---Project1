a3f1698c643703208bd5242056b800225d1a424d
Gustavo Trevisan Dini <gustavotrevisandini@gmail.com>
2018-12-29 00:19:15 -0200

    Initial Commit

---
+++ MemeMe 1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe 1.0
+//
+//  Created by Gustavo Dini on 29/12/18.
+//  Copyright © 2018 Gustavo Dini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
